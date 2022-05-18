76794c9d6d63f199ceaaf603adb94bae6456a841
SagamiYun <qiaolei19981021@163.com>
2022-05-18 11:33:18 +0800

    2022/05/18/04  demo:add music man 组件 Flutter

--- music_app/lib/pages/home/home_page.dart
+++ music_app/lib/pages/home/home_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:music_app/pages/home/header.dart';
 
-
+import 'musician_banner.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,7 +12,15 @@ class HomePage extends StatelessWidget {
     return Scaffold(
         resizeToAvoidBottomInset: false,
         body: Column(
-          children: const <Widget>[Header()],
+          children: const <Widget>[
+            Header(),
+            SizedBox(
+              height: 40,
+            ),
+            MusicianBanner(
+              title: "热榜音乐人",
+            )
+          ],
         ));
   }
 }
