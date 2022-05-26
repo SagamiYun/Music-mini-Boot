c906c3a910a1dea920d8f89f432577f81721f4fc
SagamiYun <qiaolei19981021@163.com>
2022-05-26 15:31:09 +0800

    2022/05/26/02 add: Upload assembly Vue

---
+++ music-mini-admin/src/composables/Uploader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <q-uploader :url="url" style="max-width: 300px" @added="onFileAdded"/>
+  </div>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+
+const url = ref(null);
+const files = ref([]);
+const onFileAdded = data => {
+  files.value = data;
+};
+</script>
+
+<style scoped></style>
--- music-mini-admin/src/pages/dashboard/Index.vue
+++ music-mini-admin/src/pages/dashboard/Index.vue
@@ -1,14 +1,19 @@
 <template>
   <div>欢迎光临 {{ nickname }}</div>
   <q-btn @click="logout">退出</q-btn>
+  <div class="page">
+    <CosUploader label="上传" style="width: 40vw"/>
+  </div>
 </template>
 
 <script>
 import {useStore} from 'vuex';
 import {computed} from 'vue';
+import CosUploader from '../../composables/useCosUploader.js';
 
 export default {
   name: 'Index',
+  components: {CosUploader},
   setup() {
     const store = useStore();
     return {
